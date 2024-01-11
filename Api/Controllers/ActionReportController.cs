cbb5e937aef92f1291318f5170c2122f9866de05
Mohammed7amada <m7amd7amada@gmail.com>
2024-01-11 10:00:03 +0200

    Updating

--- Api/Controllers/ActionReportController.cs
+++ Api/Controllers/ActionReportController.cs
@@ -1,6 +1,8 @@
 using Api.Filters;
+using Api.Models;
 using Microsoft.AspNetCore.Mvc;
 using Redis.OM;
+using Redis.OM.Searching;
 
 namespace Api.Controllers;
 
@@ -10,11 +12,14 @@ public class ActionReportController : ControllerBase
 {
     private readonly RedisConnectionProvider _provider;
     private readonly ActionReportFilter _reportFilter;
+    private readonly RedisCollection<LogApiRequestDetails> _actionInfoLogApiRequestDetails;
 
     public ActionReportController(RedisConnectionProvider provider, ActionReportFilter reportFilter)
     {
         _provider = provider;
         _reportFilter = reportFilter;
+        _actionInfoLogApiRequestDetails = (RedisCollection<LogApiRequestDetails>)
+            _provider.RedisCollection<LogApiRequestDetails>();
     }
 
     [HttpGet]
